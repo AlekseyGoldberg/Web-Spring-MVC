8d78eeea03e337a47bfe0655223061d127df5a47
alekseyZhizhin <zhizhin309@yandex.ru>
2022-04-27 10:05:14 +0300

    Update Request

--- HTTP/src/main/java/task1/ConnectionClient.java
+++ HTTP/src/main/java/task1/ConnectionClient.java
@@ -1,15 +1,14 @@
 package task1;
 
+import org.apache.http.NameValuePair;
+
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
 
 public class ConnectionClient implements Runnable {
     BufferedReader in;
@@ -24,53 +23,29 @@ public class ConnectionClient implements Runnable {
             var socket = serverSocket.accept();
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new BufferedOutputStream(socket.getOutputStream());
-            this.request = new Request(in);
+            this.request = new Request(in, out, validPaths);
         } catch (IOException e) {
-        } catch (InterruptedException e) {
+        } catch (InterruptedException | URISyntaxException e) {
             e.printStackTrace();
         }
     }
-//http://localhost:9999/my/?name=aleksey&age=19&name=ji
+
+    //http://localhost:9999/my/?name=aleksey&age=19&name=ji
     @Override
     public void run() {
+        List<NameValuePair> params = null;
         try {
-            // read only request line for simplicity
-            // must be in form GET /path HTTP/1.1
-            final var requestLine = in.readLine();
-            final var parts = requestLine.split(" ");
-
-            if (parts.length != 3) {
-                // just close socket
-                return;
-            }
-
-            final var path = parts[1];
-            if (!validPaths.contains(path)) {
-                out.write(("HTTP/1.1 404 Not Found\r\n"
-                        + "Content-Length: 0\r\n"
-                        + "Connection: close\r\n"
-                        + "\r\n").getBytes());
-                out.flush();
-                return;
-            }
-
-            final var filePath = Path.of(".", "public", path);
-            final var mimeType = Files.probeContentType(filePath);
-
-            final var length = Files.size(filePath);
-            out.write(("HTTP/1.1 200 OK\r\n"
-                    + "Content-Type: " + mimeType + "\r\n"
-                    + "Content-Length: " + length + "\r\n"
-                    + "Connection: close\r\n" + "\r\n")
-                    .getBytes());
-            Files.copy(filePath, out);
-            out.flush();
-            Map<String,String> params=request.getQueryParams();
-            for (Map.Entry<String,String> i:params.entrySet()){
-                System.out.println(i.getKey()+" "+i.getValue());
-            }
-        } catch (IOException | URISyntaxException exception) {
-            exception.printStackTrace();
+            params = request.getQueryParams();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
         }
+        for (NameValuePair param : params)
+            System.out.println("name: " + param.getName() + " value: " + param.getValue());
+
+        System.out.println("-----------------");
+        List<NameValuePair> listOfParam = request.getQueryParam("name");
+        System.out.println("List of params name:");
+        for (NameValuePair param : listOfParam)
+            System.out.println("name: " + param.getName() + " value: " + param.getValue());
     }
 }
--- HTTP/src/main/java/task1/Request.java
+++ HTTP/src/main/java/task1/Request.java
@@ -3,35 +3,85 @@ package task1;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Request {
-    private String requestLine;
+    private final String requestLine;
     private String startLine;
     private String headers;
     private String bodyRequest;
     private int indexEmptyLine;
+    private final List<String> validPaths;
+    private final BufferedOutputStream out;
+    private List<NameValuePair> params;
 
-    public Request(BufferedReader in) throws IOException, InterruptedException {
+    public Request(BufferedReader in, BufferedOutputStream out,List<String > validPaths) throws IOException, InterruptedException, URISyntaxException {
         this.requestLine = in.readLine();
+        this.out=out;
+        this.validPaths=validPaths;
+
+        params=new ArrayList<>();
+        params = URLEncodedUtils.parse(new URI(requestLine), "UTF-8");
 //        startLine();
 //        headers();
 //        bodyRequest();
     }
 
-    public Map<String, String> getQueryParams() throws URISyntaxException {
-        Map<String, String> params = new HashMap<>();
-        List<NameValuePair> listOfParams = URLEncodedUtils.parse(new URI(requestLine), "UTF-8");
-        for (NameValuePair nvp : listOfParams)
-            params.put(nvp.getName(), nvp.getValue());
+    public void getResponse(){
+        try {
+
+            final var parts = requestLine.split(" ");
+
+            if (parts.length != 3) {
+                // just close socket
+                return;
+            }
+
+            final var path = parts[1];
+            if (!validPaths.contains(path)) {
+                out.write(("HTTP/1.1 404 Not Found\r\n"
+                        + "Content-Length: 0\r\n"
+                        + "Connection: close\r\n"
+                        + "\r\n").getBytes());
+                out.flush();
+                return;
+            }
+
+            final var filePath = Path.of(".", "public", path);
+            final var mimeType = Files.probeContentType(filePath);
+
+            final var length = Files.size(filePath);
+            out.write(("HTTP/1.1 200 OK\r\n"
+                    + "Content-Type: " + mimeType + "\r\n"
+                    + "Content-Length: " + length + "\r\n"
+                    + "Connection: close\r\n" + "\r\n")
+                    .getBytes());
+            Files.copy(filePath, out);
+            out.flush();
+        }catch (IOException e){}
+    }
+
+    public List<NameValuePair> getQueryParams() throws URISyntaxException {
         return params;
+    }
+    public List<NameValuePair> getQueryParam(String name){
+        List<NameValuePair> listOfParam=new ArrayList<>();
+        for (NameValuePair param:params){
+            if (param.getName().equals(name))
+                listOfParam.add(param);
+        }
+        return listOfParam;
     }
 
     private void startLine() {
