5ca0034a6e2b352c44a108978298fa4dba11bc84
alekseyZhizhin <zhizhin309@yandex.ru>
2022-05-10 18:29:01 +0300

    fix

--- ServletsContainers/src/main/java/repository/PostRepository.java
+++ ServletsContainers/src/main/java/repository/PostRepository.java
@@ -3,24 +3,21 @@ package repository;
 import exception.NotFoundException;
 import model.Post;
 
+import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 public class PostRepository {
-    List<Post> listOfPost;
-
+    ConcurrentSkipListMap<Long,Post> listOfPost;
     public PostRepository(){
-        listOfPost=new LinkedList<>();
+        listOfPost= new ConcurrentSkipListMap<>();
     }
     public List<Post> all() {
-        return listOfPost;
+        return listOfPost.values().stream().toList();
     }
 
     public Optional<Post> getById(long id) {
-        Post foundPost=null;
-        for (Post post:listOfPost)
-            if (post.getId()==id)
-                foundPost=post;
+        Post foundPost=listOfPost.get(id);
         if (foundPost!=null)
             return Optional.of(foundPost);
         else
@@ -30,28 +27,25 @@ public class PostRepository {
     public Post save(Post post) {
         if (post.getId()==0){
             post.setId(listOfPost.size()+1);
-            listOfPost.add(post);
+            listOfPost.put((long)listOfPost.size()+1,post);
             return post;
         }else {
             long foundId=post.getId();
-            for (Post foundPost:listOfPost){
-                if (foundPost.getId()==foundId){
-                    foundPost.setContent(post.getContent());
+            for (Long foundPost:listOfPost.keySet()){
+                if (foundPost==foundId){
+                    listOfPost.get(foundPost).setContent(post.getContent());
                     return post;
                 }
             }
         }
         throw new NotFoundException("Not found id");
     }
-
     public Post removeById(long id) {
-        for (int i=0;i<listOfPost.size();i++){
-            if (listOfPost.get(i).getId()==id){
-                Post deletePost=listOfPost.get(i);
-                listOfPost.remove(i);
-                return deletePost;
-            }
+        Post deletePost=listOfPost.remove(id);
+        if (deletePost!=null){
+            return deletePost;
+        }else {
+            throw new NotFoundException("Not found id");
         }
-        throw new NotFoundException("Not found id");
     }
 }
